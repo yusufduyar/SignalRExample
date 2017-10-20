4966cbe98b01c50bbbca52e1783821301aacc7c3
Yusuf Duyar <yusufduyar@outlook.com>
2017-10-20 13:41:05 +0300

    Initial Commit

---
+++ SignalRExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRExampleOnCTVM.MvcApplication" Language="C#" %>
---
+++ SignalRExample/Scripts/SignalR.js
@@ -0,0 +1,13 @@
+﻿//Immediately Invoked Function Expression
+(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("IT WORKED!");
+            $.connection.myHub.server.announce("Connected!");
+        })
+        .fail(function () { alert("ERROR") });
+
+    $.connection.myHub.client.announce = function (message) {
+        $("#welcome-messages").append(message + "<br/>")
+    }
+})()
