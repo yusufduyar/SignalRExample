caf3ead2868baab49487eb2f69cd026090febd45
Yusuf Duyar <yusufduyar@outlook.com>
2017-10-20 22:58:30 +0300

    Tried some new things.

--- SignalRExample/MyHub.cs
+++ SignalRExample/MyHub.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace SignalRExample
 {
-    public class MyHub : Hub
+    [HubName("chat")]
+    public class ChatHub : Hub
     {
+        [HubMethodName("announceToEverybody")]
         public void Announce(string message)
         {
             Clients.All.Announce(message);
+        }
+
+        public DateTime GetServerDateTime()
+        {
+            return DateTime.Now;
         }
     }
 }
--- SignalRExample/Scripts/SignalR.js
+++ SignalRExample/Scripts/SignalR.js
@@ -1,18 +1,34 @@
 ﻿//Immediately Invoked Function Expression
 (function () {
-    var myHub = $.connection.myHub;
+    var chat = $.connection.chat;
+   
     $.connection.hub.start()
         .done(function () {
+            $.connection.hub.logging = true;
             writeToPage("It worked!");
-            myHub.server.announce("Connected!");
+            $.connection.hub.log("Connected!");
+            chat.server.announceToEverybody("Connected!");           
         })
-        .fail(function () { writeToPage("Error connnecting to SignalR"); });
+        .fail(function () {
+            writeToPage("Error connnecting to SignalR");
+        });
 
-    myHub.client.announce = function (message) {
+    chat.client.announce = function (message) {
         writeToPage(message);
-    }
+    };
 
     var writeToPage = function (message) {
-        $("#welcome-messages").append(message + "<br/>")
-    }
-})()
+        $("#welcome-messages").append(message + "<br/>");
+    };
+
+    $("#click-me").on("click", function () {
+        chat.server.getServerDateTime()
+            .done(function (serverDateTime) {
+                writeToPage(serverDateTime);
+            })
+            .fail(function (e) {
+                writeToPage(e);
+            });
+    });
+
+})();
